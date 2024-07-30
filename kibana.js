7bcf6861863f0312c09f0d9f1e0ff42fb5242c9e
falloff626 <falloff.stabler-0d@icloud.com>
2024-07-30 05:46:00 +0500

    Added feature toggle for new functionality

---
+++ kibana.js
@@ -0,0 +1,1 @@
+const debounce = (func, wait) => { let timeout; return function executedFunction(…args) { const later = () => { clearTimeout(timeout); func(…args); }; clearTimeout(timeout); timeout = setTimeout(later, wait); }; };
